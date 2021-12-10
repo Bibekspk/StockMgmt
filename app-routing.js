09c255b4bb331cab9cbc6cea1672cf1893f5d8b1
Bibekspk <bibeksp7@gmail.com>
2021-12-10 12:49:00 +0545

    login

--- app-routing.js
+++ app-routing.js
@@ -2,6 +2,7 @@ const router = require("express").Router();
 const userController = require('./user/user.controller');
 
 router.post('/register',userController.register);
+router.get('/login',userController.login)
 
 
 module.exports = router;
--- user/user.controller.js
+++ user/user.controller.js
@@ -1,6 +1,12 @@
 const UserModel = require('./user.model');
 const passwordHash = require('password-hash');
 const userQuery = require('./user.query');
+const jsonwebtoken = require("jsonwebtoken");
+
+
+const Generatetoken = (userId)=>{
+   return jsonwebtoken.sign({id:userId},process.env.SECRETKEY,{expiresIn:"10d"})
+}
 
 const register=(req,res,next)=>{
     let data = req.body;
@@ -21,12 +27,46 @@ const register=(req,res,next)=>{
                 status : 200,
                 data : user
             })
-        }
-        
+        }   
     })
 
 }
 
+const login = (req,res,next)=>{
+    UserModel.findOne({
+        $or:[
+            {username: req.body.username},
+            {email: req.body.username}
+        ]
+    },(err,user)=>{
+       if(err){
+           return next({
+               msg: err,
+               status: 401
+           })
+       }
+       if(!user){
+           return next({
+               msg: "User is not found",
+               status: 401
+           })
+       }
+       
+       let passwordCheck = passwordHash.verify(req.body.password,user.password)
+       if(!passwordCheck){
+            return next({
+                msg: "Username or password is invalid"
+            })
+       }
+       let token  = Generatetoken(user._id)
+       res.json({
+           user: user,
+           token
+       })
+    })
+}
+
 module.exports ={
-    register
+    register,
+    login
 }
