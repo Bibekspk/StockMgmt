550ea5e3a7ea196b403cfeaea53142d2ae5aba42
Bibekspk <bibeksp7@gmail.com>
2022-03-04 17:12:31 +0545

    sales schema creation

--- stocks/itemType.model.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const Schema = mongoose.Schema;
-
-const ItemTypeSchema=new Schema({
-    itemType:String
-})
-
-module.exports= mongoose.model('ItemType',ItemTypeSchema);
---
+++ stocks/sales.model.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const SalesSchema = new Schema({
+    billno: String,
+    items: [Object],
+    totalAmount: String,
+    salesDate: Date
+})
+
+module.exports = mongoose.model('Sales',SalesSchema);
